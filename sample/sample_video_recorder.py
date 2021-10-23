f49d97c89d8c10b8bc76823f5e9a0e1a7f9c9525
Windrow <565101079@qq.com>
2021-10-23 15:00:29 +0800

    Add gitignore.

---
+++ sample/sample_video_recorder.py
@@ -0,0 +1,9 @@
+# A sample executable to verify the feasibility of recording a pygame program to video file.
+# Reference to https://zhuanlan.zhihu.com/p/150205805
+
+import pygame
+
+
+pygame.init()
+pygame.display.set_caption("Sample Video Recorder")
+
