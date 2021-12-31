255e9fbff5e691fdf1dda7ff94cd279e2c4cb866
Yinzhe Wu <565101079@qq.com>
2021-12-31 16:59:50 +0800

    Create water.py

---
+++ source/core/water.py
@@ -0,0 +1,7 @@
+# define ocean and sea generation based on land height and sea level
+# define wind based on three-cell circulation theory, season shift, and land height
+# define heat absorption based on coordinate, season shift, and local climate
+# define evaporation rate based on heat absorption and water level
+# define rainfall ratio based on heat absorption (water transferred to lower temperature place is more likely to drop)
+# define water transmission rate based on wind and rainfall ratio
+# define river and lake generation based on 
