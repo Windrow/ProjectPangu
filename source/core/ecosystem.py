58b307a91bf6365afd74833bfef18c14f1790c5e
Yinzhe Wu <565101079@qq.com>
2021-12-31 17:14:18 +0800

    Create ecosystem.py

---
+++ source/core/ecosystem.py
@@ -0,0 +1,4 @@
+# plant object definition
+# animal object definition
+# generate plant distribution based on climate, water, and terrain
+# generate animal distribution based on plant, climate, water, and terrain
